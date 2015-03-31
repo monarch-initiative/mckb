81b82809b40fe23e9d4547a10e83c486a67a65d3
kshefchek <kshefchek@gmail.com>
2015-03-31 16:36:44 -0700

    add mygene script, ncbigene mappings to hgnc symbols

--- GraphGenerator.py
+++ GraphGenerator.py
@@ -4,6 +4,7 @@ import argparse
 import logging
 import getpass
 import sys
+import json
 
 
 def main():
@@ -16,20 +17,31 @@ def main():
                                      argparse.RawTextHelpFormatter)
     parser.add_argument('--host', '-H', type=str, default="localhost",
                         help='Location of MySQL Server')
-    parser.add_argument('--database', '-D', type=str, required=True,
-                        help='Name of database')
+    parser.add_argument('--database', '-D', type=str, help='Name of database')
     parser.add_argument('--user', '-u', help='Username')
     parser.add_argument('--password', '-p', help='Password')
+    parser.add_argument('--config', '-c', help='Config file, see example '
+                                               'formatting in conf directory')
 
     args = parser.parse_args()
 
-    # Parse test source
+    # Config file overrides command line credentials
+    # We need to refactor the Dipper config.py so it is reusable here
+    if args.config is not None:
+        credentials = json.load(open(args.config, 'r'))
+        args.host = credentials['dbauth']['g2p']['host']
+        args.database = credentials['dbauth']['g2p']['database']
+        args.user = credentials['dbauth']['g2p']['user']
+        args.password = credentials['dbauth']['g2p']['password']
+
     if args.password is None:
         if sys.stdin.isatty():
             args.password = getpass.getpass(prompt="Enter your password: ",
                                             stream=None)
         else:
             args.password = input("Enter your password: ")
+
+    # Parse test source
     logger.debug("Converting test database to triples")
     test_source = G2P(args.database, args.user, args.password, args.host)
     test_source.parse()
---
+++ scripts/README.md
@@ -0,0 +1,11 @@
+# mckb scripts
+
+### Convert HGNC Gene symbols to Entrez Gene with curie prefix NCBIGene:
+
+
+USAGE ./scripts/fetch-gene-ids.py --input ./resources/mappings/gene.tsv --ouput ./gene.tsv
+
+
+* Required python packages:
+    * [Mygene](https://github.com/monarch-initiative/dipper)
+
