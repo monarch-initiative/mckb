1df223495c4cf8932f1edc5bdbcc13e6afb1d413
kshefchek <kshefchek@gmail.com>
2015-04-27 12:17:52 -0700

    adding travis yaml configuration

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(name='mckb',
       version='0.0.1',
       packages=find_packages(),
       install_requires=['dipper', 'pymysql', 'rdflib'],
-      include_package_data=True
+      include_package_data=True,
+      dependency_links=['https://github.com/monarch-initiative/dipper/tarball/master#egg=Dipper-0.0.1']
       )
